12f87a0af1759274899f818d8ab1d2d372e5fca3
Chrisadri <c_sadri@hotmail.com>
2023-10-16 15:46:49 +1100

    Damage/Health
    added stats and health bar UI

---
+++ Assets/Scenes/ChrisFolder/Scripts/CS_DamageReactions.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CS_DamageReactions : MonoBehaviour
+{   
+
+    public float damageAmount;
+    public StatSystem.DamageType damageType; //refferencing another script (StatSystem)
+
+    public float damageRange;
+
+    public LayerMask attackableLayers; //for enabling layer to be effected
+
+
+    public void DamageBurst()
+    {
+        //get objects within radius and store to variable
+        Collider[] possibleEnemies = Physics.OverlapSphere(transform.position, damageRange, attackableLayers); //creating array for every collider the sphere connects with in a layer mask
+        //itterate through each potential target
+        if(possibleEnemies.Length > 0)
+        {
+            foreach(Collider enemy in possibleEnemies)
+            {   //check the target isn't me, this is to stop looping damaging again
+                if(enemy.gameObject != gameObject)//if not targetting myself
+                {
+                    //check the target has Stat
+                    Stats stats = enemy.GetComponent<Stats>();
+                     //deal damage to Stat
+                     if(stats != null)
+                     StatSystem.DealDamage(stats, damageType, damageAmount);
+                }
+            }
+        }
+        
+        
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    public void Die()
+    {
+        Destroy(gameObject);
+    }
+
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.DrawWireSphere(transform.position, damageRange);
+    }
+}
