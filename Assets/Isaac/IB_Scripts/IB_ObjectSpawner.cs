68fa32289d9c0ee2bd5318529ea5e57195faf208
IsaacBong <isaacbong00@gmail.com>
2023-10-23 14:45:42 +1100

    PRojectile

---
+++ Assets/Isaac/IB_Scripts/IB_ObjectSpawner.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IB_ObjectSpawner : MonoBehaviour
+{
+    public GameObject objectPrefab;
+    public Transform spawnPoint;
+    public float spawnTime;
+    private float lastSpawnTime = 0;
+    public bool isActive;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (!isActive)
+            if (Time.time - lastSpawnTime > spawnTime) 
+            {
+                Instantiate(objectPrefab, spawnPoint.position, spawnPoint.rotation);
+                lastSpawnTime = Time.time;
+            }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Isaac/IB_Scripts/IB_projectile.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class IB_projectile : MonoBehaviour
+{
+    public float moveSpeed;
+    public float deathTime;
+
+    Rigidbody rb;
+
+    public UnityEvent OnDestroy;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        Invoke("DestroySelf", deathTime);
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        rb.position += transform.forward * moveSpeed;
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        DestroySelf();
+    }
+    void DestroySelf()
+    {
+        OnDestroy.Invoke();
+        Destroy(gameObject, 1f);
+    }
+}
