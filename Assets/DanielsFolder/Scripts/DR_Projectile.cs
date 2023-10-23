9b0bcb7ae6c7a40b76e0cbeb18d0827edd16cf21
DanielRHolmesglen <daniel.roberts@holmesglen.edu.au>
2023-10-23 12:33:32 +1100

    adding templates npcs to my scene

---
+++ Assets/DanielsFolder/Scripts/DR_Projectile.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class DR_Projectile : MonoBehaviour
+{
+    public float moveSpeed;
+    public float deathTime;
+
+    Rigidbody rb;
+
+    public UnityEvent OnDestroy;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        Invoke("DestroySelf", deathTime);
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        rb.position += transform.forward * moveSpeed;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        DestroySelf();
+    }
+
+    void DestroySelf()
+    {
+        OnDestroy.Invoke();
+        Destroy(gameObject, 0.1f);
+    }
+}
