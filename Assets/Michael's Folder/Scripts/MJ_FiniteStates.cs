aee2369dccce825c9209a0365d2ab1f8e734bf7d
Shiroinu25 <gunnzlinger@gmail.com>
2023-10-24 13:47:52 +1100

    Added scripts

---
+++ Assets/Michael's Folder/Scripts/MJ_FiniteStates.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MJ_FiniteStates : MonoBehaviour
+{
+    /// <summary>
+    /// FiniteStateMachine for NPCs
+    /// </summary>
+
+    public float sightRange;
+
+    public IEnumerator States {IDLE, CHASING, ATTACKING, };
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
