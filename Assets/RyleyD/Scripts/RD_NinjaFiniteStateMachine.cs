5d46c6c1d3417bfe81b748c480f4c550bf47c5f9
RyleyDodd <125518408+RyleyDodd@users.noreply.github.com>
2023-11-22 12:32:16 +1100

    Pie
    More stuff added

--- Assets/RyleyD/Scripts/RD_NinjaFiniteStateMachine.cs
+++ Assets/RyleyD/Scripts/RD_NinjaFiniteStateMachine.cs
@@ -1,6 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
+using UnityEngine.VFX;
 
 public class RD_NinjaFiniteStateMachine : MonoBehaviour
 {
@@ -8,15 +10,30 @@ public class RD_NinjaFiniteStateMachine : MonoBehaviour
     public float sightRange;
     public float meleeRange;
     public float rangedRange;
+    //variables for attack timers;
+    public float timeBetweenAttacks;
+    float lastAttack;
+
     public Transform player;
-    private UnityEngine.AI.NavMeshAgent agent;
+    private NavMeshAgent agent;
+
+    private Animator anim;
+    public Collider weapon1, weapon2, weapon3;
+    public VisualEffect smokeBombParticle;
+
+    //patrolling
+    public List<Vector3> waypoints = new List<Vector3>();
+    private int waypointIndex;
+
+
+    [HideInInspector]
     public Color sightColor;
     #endregion
 
     #region States
     /// Declare states. If you add a new state to your character,
     /// remember to add a new States enum for it.
-    public enum States { IDLE, ROAMING, CHASING, MELEEATTACK, RANGEDATTACK, DODGE, FLEEING }
+    public enum States { IDLE, PATROLLING, CHASING, MELEEATTACK, RANGEDATTACK, DODGE, FLEEING, DEATH }
     public States currentState;
     #endregion
 
@@ -28,7 +45,27 @@ public class RD_NinjaFiniteStateMachine : MonoBehaviour
     }
     private void Start()
     {
-        agent = GetComponent<UnityEngine.AI.NavMeshAgent>();
+        GetComponent<Stats>().OnDamaged.Invoke();
+        agent = GetComponent<NavMeshAgent>();
+
+        anim = GetComponentInChildren<Animator>();
+
+        //turn hitboxes off by default
+        weapon1.enabled = false;
+        weapon2.enabled = false;
+        weapon3.enabled = false;
+
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+
+        //collect the waypoint positions
+        foreach (Transform item in transform)
+        {
+            if (item.tag == "Waypoint")
+            {
+                waypoints.Add(item.position);
+            }
+        }
+        lastAttack = 0;
         //start the fsm
         StartCoroutine(EnemyFSM());
     }
@@ -50,6 +87,7 @@ public class RD_NinjaFiniteStateMachine : MonoBehaviour
     {
         //ENTER THE IDLE STATE >
         //put any code here that you want to run at the start of the behaviour
+        float timer = 0; //create a number to count from to track if idle should transition;
 
         Debug.Log("Alright, seems no evil Player is around, I can chill!");
 
@@ -57,12 +95,11 @@ public class RD_NinjaFiniteStateMachine : MonoBehaviour
         //put any code here you want to repeat during the state being active
         while (currentState == States.IDLE)
         {
-            //check player distance
-            if (Vector3.Distance(transform.position, player.position) < sightRange)
-            {
-                //change state
-                currentState = States.CHASING;
-            }
+            //check for player and count up untile idle time has run out
+            if (IsInRange(meleeRange)) currentState = States.MELEEATTACK;
+            else if (IsInRange(sightRange)) currentState = States.CHASING;
+            else timer += Time.deltaTime;
+            if (timer > 5) currentState = States.PATROLLING;
             yield return new WaitForEndOfFrame();
         }
 
@@ -98,29 +135,120 @@ public class RD_NinjaFiniteStateMachine : MonoBehaviour
         Debug.Log("Oh no I see the player!");
     }
 
-    IEnumerator ROAMING()
+    IEnumerator PATROLLING()
     {
-        //ENTER THE ROAMING STATE >
+        //ENTER THE Chasing STATE >
+        //put any code here that you want to run at the start of the behaviour
+        agent.SetDestination(waypoints[waypointIndex]);
+        Debug.Log("I'ma Get Ya!");
 
-        Debug.Log("Where did they go?");
-
-        //UPDATE ROAMING STATAE >
-        while (currentState == States.ROAMING)
+        //UPDATE Chasing STATE >
+        //put any code here you want to repeat during the state being active
+        while (currentState == States.PATROLLING)
         {
-            agent.SetDestination(player.position);
-
-            if (Vector3.Distance(transform.position, player.position) > sightRange)
+            if (IsInRange(meleeRange) && Time.time - lastAttack > timeBetweenAttacks) currentState = States.MELEEATTACK;
+            else if (IsInRange(sightRange)) currentState = States.CHASING;
+            else
             {
-                currentState = States.IDLE;
-                Debug.Log("aaaargh they're too fast!");
+                if (!agent.pathPending && agent.remainingDistance <= agent.stoppingDistance)
+                {
+                    waypointIndex = waypointIndex + 1 % waypoints.Count;
+                }
+
+                agent.SetDestination(waypoints[waypointIndex]);
             }
+
             yield return new WaitForEndOfFrame();
         }
 
+        //EXIT IDLE STATE >
+        //write any code here you want to run when the state is left
 
+        Debug.Log("Oh no I see the player!");
+    }
+    IEnumerator MELEEATTACK()
+    {
+        //ENTER THE Chasing STATE >
+        //put any code here that you want to run at the start of the behaviour
+
+        Debug.Log("I'ma Get Ya!");
+        agent.SetDestination(transform.position);
+        agent.updateRotation = false;
+        lastAttack = Time.time;
+        //roll a number to pick which attack to run
+        int attackType = Random.Range(0, 100);
+
+        if (attackType < 70)//70%chance to run the basic attack
+        {
+            anim.SetTrigger("BasicAttack");//run the attack animation
+            weapon1.enabled = true;//enable the damage collider
+            yield return new WaitForSeconds(4); //wait for the animation to end
+            weapon1.enabled = false;
+        }
+        else if (attackType > 70)//remaining 30% chance to run the special attack
+        {
+            anim.SetTrigger("RoarAttack");//run the attack animation
+            yield return new WaitForSeconds(1.5f); //wait for first hitbox
+            weapon2.enabled = true;//enable the damage collider
+            yield return new WaitForSeconds(0.1f); //wait for first hitbox
+            weapon3.enabled = true;
+
+            yield return new WaitForSeconds(0.1f); //wait for first hitbox to finish
+
+            weapon2.enabled = false;
+
+            yield return new WaitForSeconds(3); //wait for the animation to end
+            weapon3.enabled = false;
+        }
+
+
+
+        //EXIT IDLE STATE >
+        //write any code here you want to run when the state is left
+        agent.updateRotation = true;
+        if (IsInRange(sightRange)) currentState = States.CHASING;
+        else currentState = States.PATROLLING;
+
+
+        Debug.Log("Oh no I see the player!");
+    }
+    IEnumerator DEATH()
+    {
+        //ENTER THE Chasing STATE >
+        //put any code here that you want to run at the start of the behaviour
+        weapon1.enabled = false;
+        weapon2.enabled = false;
+        weapon3.enabled = false;
+        agent.speed = 0;
+        agent.SetDestination(transform.position);
+        anim.SetTrigger("Death");
+        yield return new WaitForSeconds(2f); //wait for the animation to end
+
+        SkinnedMeshRenderer[] models = GetComponentsInChildren<SkinnedMeshRenderer>();
+        foreach (SkinnedMeshRenderer model in models)
+        {
+            model.enabled = false;
+        }
+        MeshRenderer[] meshs = GetComponentsInChildren<MeshRenderer>();
+        foreach (MeshRenderer mesh in meshs)
+        {
+            mesh.enabled = false;
+        }
+        Destroy(gameObject, 2);
+
+
+
+        Debug.Log("Oh no I see the player!");
     }
     #endregion
 
+    bool IsInRange(float range)
+    {
+        if (Vector3.Distance(player.position, transform.position) < range)
+            return true;
+        else
+            return false;
+    }
     void OnDrawGizmosSelected()
     {
         Gizmos.color = sightColor;
