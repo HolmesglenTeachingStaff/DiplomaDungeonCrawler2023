1912c9ee132abfe48170d0cb242fb4262e594b25
RyleyDodd <125518408+RyleyDodd@users.noreply.github.com>
2023-11-24 17:33:34 +1100

    Stuff

--- Assets/RyleyD/Scripts/RD_AmaterasuHealing.cs
+++ Assets/RyleyD/Scripts/RD_AmaterasuHealing.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.VFX;
 
 public class RD_AmaterasuHealing : MonoBehaviour
 {
@@ -9,8 +10,10 @@ public class RD_AmaterasuHealing : MonoBehaviour
     public float healAmount = 10f;
     public float newMax = 200f;
 
+    [SerializeField] VisualEffect burstPrefab;
+
     //get a reference to the GameObject
-    private void OnTriggerEnter(Collider other)
+    private void OnTriggerStay(Collider other) //changed from enter to stay so is repetively called while inside the zone instead of only once.
     {
         if (Input.GetKeyDown(KeyCode.E))
             //check if the object has the correct tag to be a target
@@ -23,8 +26,19 @@ public class RD_AmaterasuHealing : MonoBehaviour
             statsScript.currentHealth += healAmount;
             statsScript.maxHealth += statsScript.maxHealth * 0.5f;
             statsScript.maxHealth *= 1.5f;
+            statsScript.OnBuffRecieved.Invoke(); //run the Onbuffrecieved event so that the UI can change in response when that feature is implemeted.
+            //spawn the particle
+            SpawnParticle();
             }
     }
-
+    void SpawnParticle()
+    {
+        //istantiate a new particle
+        VisualEffect newBurstEffect = Instantiate(burstPrefab, transform.position, transform.rotation);
+        //play the particle
+        newBurstEffect.Play();
+        //destroy the particle
+        Destroy(newBurstEffect.gameObject, 5f);
+    }
 
 }
